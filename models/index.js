277fd5aee359ca18eb7bcc3f3f2c59481d8956fd
night <yseddiq@gmail.com>
2022-09-04 18:51:02 -0400

    added ability to make posts, update post titles, and delete a post

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create assoications.
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User,{
+    foreinKey: 'user_id'
+});
 
 module.exports = { User,Post };
