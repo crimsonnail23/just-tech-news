28d7f9f14c803f09004dd869464128bb46b64418
night <yseddiq@gmail.com>
2022-09-06 16:55:04 -0400

    added basic login and logout functionality

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Superman is Clark Kent',
+    cooke: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(session(sess));
 
 //turn on routes.
 app.use(routes);
