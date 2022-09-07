4a82f40b7c4f565d692de4dda6df7a73818e537d
night <yseddiq@gmail.com>
2022-09-07 17:18:32 -0400

    added ability to upvote and comment from the front end, and dynamically render Login and Logout depending on if user is logged in or not

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,16 +13,18 @@ router.get('/', (req,res)=>{
 
 // POST a comment.
 router.post('/', (req,res)=>{
-    Comment.create({
+  if(req.session) {
+      Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
+        //use the ID from the session.
+        user_id: req.session.user_id,
         post_id: req.body.post_id
     })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err=>{
         console.log(err);
         res.status(400).json(err);
-    })
+    })}
 });
 
 //delete a comment
